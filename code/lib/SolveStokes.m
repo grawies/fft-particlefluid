fbeb471b6c3e2d0da5e785b657aa3824237a3065
Samuel Ljungdahl Zackrisson <samuel.zackrisson@gmail.com>
2015-03-25 15:00:24 +0100

    review constants

--- code/lib/SolveStokes.m
+++ code/lib/SolveStokes.m
@@ -4,10 +4,10 @@ function [u,v,w] = SolveStokes(S,P,F)
     F.fhy = fftns(F.fy);
     F.fhz = fftns(F.fz);
     
-    kdotfh = (S.k1.*F.fhx + S.k2.*F.fhy + S.k3.*F.fhz).*S.C2;
+    kdotfhdiv = (S.k1.*F.fhx + S.k2.*F.fhy + S.k3.*F.fhz).*S.C2;
 
-    u = ifftns(F.fhx.*S.C1 - S.k1.*kdotfh);
-    v = ifftns(F.fhy.*S.C1 - S.k2.*kdotfh);
-    w = ifftns(F.fhz.*S.C1 - S.k3.*kdotfh);
+    u = ifftns(F.fhx.*S.C1 - S.k1.*kdotfhdiv);
+    v = ifftns(F.fhy.*S.C1 - S.k2.*kdotfhdiv);
+    w = ifftns(F.fhz.*S.C1 - S.k3.*kdotfhdiv);
 
 end
