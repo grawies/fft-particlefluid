4d95f33854bebf3d87342944ade56a0d3f1bd525
Samuel Ljungdahl Zackrisson <samuel.zackrisson@gmail.com>
2015-05-10 12:25:41 +0200

    change plot summary to show comparison of different regularizations

--- code/scripts/PlotProfileSummary.m
+++ code/scripts/PlotProfileSummary.m
@@ -1,15 +1,28 @@
 LocalInit(0);
-
+ 
 %[x,p,f,u] = Get1DProfile(2*pi, 128, 3, 'triangle',8);
-[x,p,f,u] = Get1DProfile(2*pi, 128, 3, 'spline', [1 5]);
+L = 2.0;
+N = 128;
+M = 3;
+[~,~,ftS,utS] = Get1DProfile(L, N, M, 'triangle', [4 5]);
+[~,~,ftL,utL] = Get1DProfile(L, N, M, 'spline', [8 5]);
+[~,~,fsSS,usSS] = Get1DProfile(L, N, M, 'spline', [1 2]);
+[~,~,fsSM,usSM] = Get1DProfile(L, N, M, 'spline', [1 4]);
+[x,p,fsSL,usSL] = Get1DProfile(L, N, M, 'spline', [1 6]);
 
+%% plot the velocity field
+figure(1)
+set(gca,'YDir','reverse'); hold all
 plot(p,zeros(max(size(p)),1),'k*');
-hold on;
-[ax,hline1,hline2] = plotyy(x,-f,x,-u);
-%originLim = get(
-%set(hLine1, 'Ylim', originlim);
-%set(hLine1,'Color', 'r');
-%set(hLine2,'Color', 'b');
+plot(x,usSS);
+plot(x,usSM);
+plot(x,usSL);
+plot(x,utS);
+plot(x,utL);
+
+%% plot the profiles
+figure(2)
+plot(p,zeros(max(size(p)),1),'k*');
 
 xlabel('z-value','FontSize',12);
 
